cff841d45847999707de6f0920389824138d51be
Tuugii <tuugii@iMac2021-08s-iMac.local>
2022-03-04 16:51:14 +0800

    12-styledBorder

--- src/components/styledBorder.jsx
+++ src/components/styledBorder.jsx
@@ -1,10 +1,12 @@
 import styled from "styled-components";
 
 export const StyledBorder = styled.div`
-    border: 1px solid ${({bc}) => bc};
-    border-radius: ${({radius}) => radius};
-    overflow: hidden;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  width: ${({ width }) => width};
+  height: ${({ height }) => height};
+  border: 1px solid ${({ bc }) => bc};
+  border-radius: ${({ radius }) => radius};
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
