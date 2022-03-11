9770679eab68028bfd07e12b7ae7ab13f720e187
Orgil <F4stella@CC0096.local>
2022-03-11 17:31:27 +0800

    #19-Position-component

--- src/components/position.jsx
+++ src/components/position.jsx
@@ -1,7 +1,11 @@
 import styled from 'styled-components';
 
 const Position = styled.div`
-  position: ${props => props.pos};
+  position: ${(props) => props.pos};
+  top:${(props)=> props.size[0]};
+  left:${(props)=> props.size[1]};
+  bottom:${(props)=> props.size[2]};
+  right:${(props)=> props.size[3]};
 `;
 
 export default Position;
