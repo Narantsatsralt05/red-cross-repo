aec551ef7cb256cc9a6a554ac64dfa8381ad7fda
Tuugii <tuugii@iMac2021-08s-iMac.local>
2022-03-14 16:10:06 +0800

    12-StyledBorder

--- src/components/styledBorder.jsx
+++ src/components/styledBorder.jsx
@@ -3,7 +3,11 @@ import styled from "styled-components";
 export const StyledBorder = styled.div`
   width: ${({ width }) => width};
   height: ${({ height }) => height};
-  border: 1px solid ${({ bc }) => bc};
+  border: ${({ border }) => border};
+  border-left: ${({borderLeft}) => borderLeft};
+  border-right: ${({borderRight}) => borderRight};
+  border-bottom: ${({borderBottom}) => borderBottom};
+  border-top: ${({borderTop}) => borderTop};
   border-radius: ${({ radius }) => radius};
   overflow: hidden;
   display: flex;
