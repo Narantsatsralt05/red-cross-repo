a6b081045d16f7f6782e80e4b79cf2c01cc339e5
Tuugii <tuugii@iMac2021-08s-iMac.local>
2022-03-21 17:13:38 +0800

    46-firebaseSetUp

--- src/components/stack.jsx
+++
@@ -1,9 +0,0 @@
-import style from 'styled-components'
-
-const Stack = style.div`
-    display:flex;
-    flex-direction:row;
-    justify-content:space-between;
-`
-
-export default Stack
--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,6 +1,3 @@
 export * from "./useCollection";
 export * from "./useDocument";
 export * from "./firebase";
-export * from "./setDocument";
-export * from "./addDocument";
-export * from "./deleteDocument";
