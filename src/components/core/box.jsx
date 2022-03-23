afd9bc3b9e134c960ae00c5feda50275d1938569
Tuugii <tuugii@iMac2021-08s-iMac.local>
2022-03-23 18:30:15 +0800

    50-box

---
+++ src/components/core/box.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const Box = ({ children, url, width, height }) => {
+
+const Container = styled.div`
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: auto;
+  width: auto;
+`;
+
+ const InnerContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+`;
+
+ const Image = styled.div`
+  width: ${({ width }) => width};
+  height: ${({ height }) => height};
+  overflow: hidden;
+  position: relative;
+  border-radius: 1px solid transparent;
+  background-image: url(${({ url }) => url});
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-position-x: center;
+`;
+
+  return (
+    <Container>
+      <Image url={url} width={width} height={height}>
+        <InnerContainer>{children}</InnerContainer>
+      </Image>
+    </Container>
+  );
+};
