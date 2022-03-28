7b58c789df5ca9b8140d87b1dbe36aef1aa4be7f
Mirgalim <mirgalim@iMac-20s-iMac.local>
2022-03-28 19:06:54 +0800

    create styled-select-component

--- src/components/select.jsx
+++ src/components/select.jsx
@@ -1,35 +1,8 @@
 import React, { Component, useState } from "react";
 import PropTypes from "prop-types";
 import styled, { ThemeProvider } from "styled-components";
-
-
-const StyledUl = styled.ul`
-    list-style-type: none;
-    margin-top: 5px;
-    /* overflow: hidden; */
-    background-color: #333;
-    padding: 0px;
-    background-color: white;
-    color: black;
-    font-weight: bold;
-    width: 300px;
-    border: 0.5px solid black;
-    border-radius: 4px;
-    height: 25px;
-    font-size: 12px;
-    display: flex;
-    /* justify-content: center; */
-    align-items: start;
-`;
-
-const StyledLi = styled.li`
-    float: left;
-    height: 100%;
-    padding: 10px;
-    &:hover {
-        background-color: red;
-    }
-`;
+import ArrowDown from "./icons/ArrowDown.jsx";
+import { Stack, Text } from "../components";
 
 const DropDownContent = styled.div`
     display: block;
@@ -37,22 +10,19 @@ const DropDownContent = styled.div`
     min-width: 160px;
     z-index: 1;
     background-color: black;
+    border: 0.4px solid gray;
+    border-radius: 5px;
+    background-color: white;
 `;
 
-const DropDownLi = styled(StyledLi)`
-    display: inline-block;
-    &:hover ${DropDownContent} {
-        display: block;
-    }
-`;
 
-const StyledA = styled.a`
+const StyledATag = styled.a`
     display: inline-block;
     text-align: center;
     text-decoration: none;
 `;
 
-const SubA = styled(StyledA)`
+const ToUseOptions = styled(StyledATag)`
     text-decoration: none;
     display: block;
     text-align: left;
@@ -60,42 +30,64 @@ const SubA = styled(StyledA)`
     padding: 10px;
     color: black;
     width: 300px;
-    height: 35px;
-    border-radius: 4px;
-    border: 0.5px solid black;
+    border-radius: 10%;
+    &:hover {
+        background-color: #d9e6f2;
+        border-radius: 4px;
+        border: 0.4px solid ##d9e6f2;
+    };
 `;
-const But = styled.button`
-    width:300px;
+const StyledSelect = styled.div`
+    width: 300px;
     height: 25px;
     background-color: white;
-    border: 0.5px solid gray;
+    border: 0.4px solid gray;
     border-radius: 4px;
-`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+const Select = ({ value, onClick }) => {
+    return (
+        <Stack>
+            <StyledSelect onClick={onClick}>
+                <Text style={{ marginLeft: '10px' }}>{value}</Text>
+                <div style={{ width: '30px', height: '25px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                    <ArrowDown />
+                </div>
+            </StyledSelect>
+        </Stack>
+    )
+}
 const Menu = (props) => {
     const [state, setState] = useState(false);
-const a = () =>{
-    setState(!state)
-}
-    const handleClick  = (action) => {
+    const Click = () => {
+        setState(!state)
+    }
+    const [options, setOptions] = useState('')
+    const handleClick = (action) => {
+        setOptions(action.text)
         if (!action) return;
-    
         if (props.onClick) props.onClick(action);
     }
-
     return (
         <>
-        <But onClick={a} >Дарна уу</But>
-            {state ? <DropDownContent>
-                <SubA onClick={() => handleClick(state)}>
-                    Aaaaaaaa
-                </SubA>
-                <SubA onClick={() => handleClick("Link2")}>
-                    bbbbbb
-                </SubA>
-                <SubA onClick={() => handleClick("Link3")}>
-                    cccccc
-                </SubA>
-            </DropDownContent > : <></>}
+            <div>
+                <Select type="button" placeholder={options} value={options} onClick={Click} rightIcon={ArrowDown}>
+                </Select>
+
+            </div>
+            {state &&
+                    <DropDownContent onClick={Click} >
+                        {
+                            props.arr.map(Element =>
+                                <ToUseOptions onClick={() => handleClick(Element)}>
+                                    {Element.text}
+                                </ToUseOptions>
+                            )
+                        }
+                    </DropDownContent > }
         </>
     )
 }
