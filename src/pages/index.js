e1ec3e937c536c5d117bb1e81712f2676f850a55
Bilguun2004 <bilguun8689@gmail.com>
2022-03-30 18:21:58 +0800

    UserCard Component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import { useAuthContext } from '../providers/authContext';
 import { useRouter } from 'next/router';
@@ -15,17 +14,3 @@ export default function Home() {
   }, [user]);
   return <>Redirecting...</>;
 }
-=======
-import React, { useState } from 'react';
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../../styles/Home.module.css';
-import { Box } from '../components';
-import {Usercard} from "../components"
-import male from "../assets/icon/male.svg"
-export default function Home() {
-  return (
-    <Usercard greenbadge={true} greybadge={true}  bluebadge={true}profile={'https://uxwing.com/wp-content/themes/uxwing/download/21-medical-science-lab/doctor-male.png'}/>
-  );
-}
->>>>>>> 3607013 (userCard component)
