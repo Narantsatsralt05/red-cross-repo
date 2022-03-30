7a1254e10fa49603ae52f1813890096caf1ae067
Enkhjin0816 <87753829+Enkhjin0816@users.noreply.github.com>
2022-03-30 19:09:04 +0800

    35 create data shower (#69)
    * 35 created data shower component
    
    * created create last-added-person table #36
    
    * conflic zassan
    
    Co-authored-by: Enkhjin <enkhjin@iMAC2021-05s-iMac.local>
    Co-authored-by: Gereltuya Munkhzaya <70522395+GereltuyaMz@users.noreply.github.com>

---
+++ src/components/common/styled-component.jsx
@@ -0,0 +1,51 @@
+import styled from 'styled-components'
+// alignItems justifyContent alignVertical 
+const StackQueue = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    height: ${({ height }) => height};
+    gap: ${({ gap }) => gap};
+    justify-content:${props => props.justifyContent};
+    align-items: ${props => props.alignItems};
+    flex-direction:${props => props.direction};
+`;  
+
+const Image = styled.div`
+    width: ${props => props.width};
+    height: ${props => props.height};
+    background: url(${props => props.url});
+    background-repeat: no-repeat;
+    background-size: cover
+`
+
+const Color = {
+    'blue': '#0066B3',
+    'grey': '#626262',
+    'orange': '#DD5D00',
+    'red': '#D90000'
+}
+
+const Text = styled.text`
+    font-size: ${props => props.number ? '36px' : '12px'};
+    font-weight: 700;
+    color: white;
+    width: 50%;
+    text-align: left;
+    padding-left: ${props => props.number ? '10px' : '2px'}
+`
+
+const DataCon = styled.div`
+    padding: 5px;
+    padding-right: 0px;
+    padding-right: 1px;
+    height: 50px;
+    width: 200px;
+    border-radius: 5px;
+    background-color: ${props => props.color ? Color[props.color] : Color['blue']};
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-around;
+`
+
+export { StackQueue , Image, DataCon, Text }
