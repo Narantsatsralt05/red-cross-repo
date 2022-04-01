d3cbd3fa3a874bb74b0a5de6f2dce8ecd630b0dd
Tuugii <87753063+Tuugii0507@users.noreply.github.com>
2022-04-01 15:55:03 +0800

    72 forgot pass page (#73)
    * [web] ESlint (#49)
    
    * 48ESlint
    
    * 48ESlint
    
    * 48ESlint
    
    Co-authored-by: nasanbat II <nasaa@iMac2021-07s-iMac.local>
    
    * forgotPassPageFinished
    
    Co-authored-by: Nasaa1234 <96171370+Nasaa1234@users.noreply.github.com>
    Co-authored-by: nasanbat II <nasaa@iMac2021-07s-iMac.local>
    Co-authored-by: Tuugii <tuugii@iMac2021-08s-iMac.local>

---
+++ src/pages/forgotPass.jsx
@@ -0,0 +1,112 @@
+import { Button, Margin, NotificationModal, Position, Stack, StyledInput, Text } from '../components';
+import React, { useState } from 'react';
+import Background from '../assets/image/forgotPassBg.png';
+import Image from 'next/image';
+import Logo from '../assets/icon/logo.svg';
+import { useAuthContext } from '../providers/authContext';
+
+const Login = () => {
+  const [form, setForm] = useState({
+    email: '',
+  });
+  const [event, setEvent] = useState('');
+
+  const handler = (event) => {
+    // changing the state to the name of the key
+    // which is pressed
+    setEvent(event.key);
+  };
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.id]: e.target.value });
+  };
+  const { forgetPass, forgotPassError } = useAuthContext();
+
+  if (event === 'Enter') {
+    forgetPass(form.email);
+  }
+  return (
+    <Stack direction="row" width="100%" height="100vh">
+      <Stack direction="column" alignItems="center" width="35%" height="100vh" bg="#fff">
+        <Stack height="15vh" width="35%" />
+        <Image src={Logo} width={90} height={90} />
+        <Margin size={[30, 0, 0, 0]}>
+          <Text color="#0066B3" fontSize="0.9vw" cursor="pointer">
+            МОНГОЛЫН УЛААН ЗАГАЛМАЙ НИЙГЭМЛЭГ
+          </Text>
+        </Margin>
+        <Margin size={[10, 0, 0, 0]}>
+          <Text color="#7A7A7A" fontSize="0.9vw" cursor="pointer">
+            Цахим мэдээллийн систем
+          </Text>
+        </Margin>
+        <Margin size={[10, 0, 0, 0]}>
+          <Stack width="30vw" height="2px" bg="#F5F5F5" />
+        </Margin>
+        <Margin size={[30, 0, 0, 0]}>
+          <Text color="black" fontSize="1.8vw" cursor="pointer">
+            НУУЦ ҮГ СЭРГЭЭХ
+          </Text>
+        </Margin>
+        <>
+          <Margin size={[40, 0, 0, 0]} />
+          <Stack width="67%" justifyContent="flex-start">
+            <Text color="#1B1B1B" fontSize="0.8vw" cursor="pointer">
+              Бүртгэлтэй цахим шуудан
+            </Text>
+          </Stack>
+          <Margin size={[10, 0, 0, 0]}>
+            <StyledInput
+              color="#1B1B1B"
+              border="2px solid #D0D0D0"
+              width="23.5vw"
+              height="4.3vh"
+              size="1.2vw"
+              placeholder="и-мэйл"
+              onChange={handleChange}
+              value={form.email}
+              id="email"
+              type="text"
+            />
+            {form.email.length >= 10 ? (
+              forgotPassError ===
+              'Firebase: There is no user record corresponding to this identifier. The user may have been deleted. (auth/user-not-found).' ? (
+                <Text color="red">и-мэйл бүртгэлгүй байна.</Text>
+              ) : (
+                ''
+              )
+            ) : (
+              ''
+            )}
+          </Margin>
+        </>
+        <Margin size={[30, 0, 0, 0]}>
+          <Button
+            width="23.5vw"
+            height="4.3vh"
+            bc="1px solid #0066B3"
+            borderRadius="5px"
+            bgColor="#0066B3"
+            color="#fff"
+            onClick={() => forgetPass(form.email)}
+          >
+            <Text color="#fff" cursor="pointer" fontSize="1.2vw" onClick={() => forgetPass(form.email)}>
+              НУУЦ ҮГ СЭРГЭЭХ
+            </Text>
+          </Button>
+        </Margin>
+      </Stack>
+      <Position position="fixed" bottom="3vh" left="4vw">
+        <Text color="#757575" fontFamily="Roboto" fontSize="0.8vw">
+          @ 2018-2021 Монголын улаан загалмай нийгэмлэг
+        </Text>
+      </Position>
+      <NotificationModal bg="#fff" />
+      <Stack width="65%" height="100vh">
+        <Image src={Background} width={1700} height={300} />
+      </Stack>
+    </Stack>
+  );
+};
+
+export default Login;
