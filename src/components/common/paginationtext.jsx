25ed6b61b0c07537abbce4dba3e9069e46ce7b2c
Bilguun2004 <86461760+Bilguun2004@users.noreply.github.com>
2022-04-01 18:36:54 +0800

    76 pagination component test (#84)
    * Pagination component
    
    * pagination component
    
    * Pagination component
    
    * Pagination
    
    Co-authored-by: Gereltuya Munkhzaya <70522395+GereltuyaMz@users.noreply.github.com>

---
+++ src/components/common/paginationtext.jsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
+import useState from 'react';
+
+export default function Page() {
+  const [page, setPage] = React.useState(1);
+  const handleChange = (event, value) => {
+    setPage(value);
+  };
+  return (
+    <Stack spacing={2}>
+      <div>{page}</div>
+      <Pagination count={10} page={page} onChange={handleChange} variant="outlined" shape="rounded" />
+    </Stack>
+  );
+}
