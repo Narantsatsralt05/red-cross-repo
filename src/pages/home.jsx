a2f451572ac3f1c90f923e2b214a46515c610271
Bilguun2004 <bilguun8689@gmail.com>
2022-04-01 16:00:19 +0800

    Pagination

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import Page from "../components/common/pagination"
+
 const Home = () => {
 
     return <>
-        <Page/>
+    Thats home 
     </>
 }
 export default Home;
