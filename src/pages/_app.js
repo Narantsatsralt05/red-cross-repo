d4c72b5283223d17b71f36a3e78ebd6d4440d291
Gereltuya Munkhzaya <gegiimz96@gmail.com>
2022-04-06 12:27:57 +0800

    fix conflicts

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,15 +1,8 @@
-<<<<<<< HEAD
 import '../../styles/globals.css';
-// import Test from '../test';
-=======
-/* eslint-disable */
-import '../../styles/globals.css'
->>>>>>> 011b8a4 ([web] ESlint (#49))
 import { ThemeProvider } from '../theme/theme';
 import { AuthProvider } from '../providers/authContext';
 
 function MyApp({ Component, pageProps }) {
-<<<<<<< HEAD
   return (
     <AuthProvider>
       <ThemeProvider>
@@ -18,14 +11,6 @@ function MyApp({ Component, pageProps }) {
     </AuthProvider>
   );
 }
-<<<<<<< HEAD
 
 export default MyApp;
-=======
-  return <ThemeProvider ><Component {...pageProps} /></ThemeProvider>
-}
-export default MyApp
->>>>>>> 011b8a4 ([web] ESlint (#49))
-=======
-export default MyApp
->>>>>>> dc86f28 (designed folder structure)
+
