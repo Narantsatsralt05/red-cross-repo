5842d07fa938c7ad7d27c0b31324f65a35ad835a
Bilguun2004 <86461760+Bilguun2004@users.noreply.github.com>
2022-04-08 17:42:37 +0800

    theme-provider (#100)

---
+++ src/theme/color.jsx
@@ -0,0 +1,22 @@
+export const color = {
+    primary:{
+        blue:"##0066B3",
+        red:'#ED1D24',
+        white:'#FFFFFF',
+        black:'#000000',
+        blue:'#0066B3',
+    },
+    secondary: {
+      green:'#1A9461',
+      orange:'#DD5D00',
+      red:'#ED1D24',
+      white:'#D1D1D1',
+      black:'#424242',
+      blue:'#0085FF',
+     },
+     disabled: {
+        black:'#A9A9A9',
+        white:'#e2e2e2',
+        blue:'#0066B3CC',
+     },
+  };
