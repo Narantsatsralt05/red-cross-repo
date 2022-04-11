44116798c557b96797e1bdf7954b9980fcfa9efd
Mirgalim <mirgalim@iMac-20s-iMac.local>
2022-04-11 16:32:11 +0800

    shadow-styled-component

---
+++ src/components/core/shadow.jsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const ShadowDown = styled.div`
+    border: 0.5px solid;
+    padding: 5px;
+    box-shadow: 0px 2px #d8d7d8;
+    border-radius: 4px;
+`
+export const ShadowTop = styled.div`
+    border: 0.5px solid;
+    padding: 5px;
+    box-shadow: 0px 2px #d8d7d8;
+    border-radius: 4px;
+`
+export const ShadowRight = styled.div`
+    border: 0.5px solid;
+    padding: 5px;
+    box-shadow: 0px 2px #d8d7d8;
+    border-radius: 4px;
+`
+export const ShadowLeft = styled.div`
+    border: 0.5px solid;
+    padding: 5px;
+    box-shadow: 0px 2px #d8d7d8;
+    border-radius: 4px;
+`
