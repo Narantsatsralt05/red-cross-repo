c86d40181dd9599d373880a87ac913c5e5691ac3
Mirgalim <97152700+Mirgalim@users.noreply.github.com>
2022-04-13 16:53:49 +0800

    Shadow styled component (#111)
    * shadow-styled-component
    
    * shadow styled component
    
    Co-authored-by: Mirgalim <mirgalim@iMac-20s-iMac.local>

---
+++ src/components/core/shadow.jsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const ShadowBottom = styled.div`
+    box-shadow: 0px 15px 10px -15px #111; 
+`
+export const ShadowTop = styled.div`
+    box-shadow: 0px -15px 10px -15px #111; 
+`
+export const ShadowRight = styled.div`
+    box-shadow: 15px 0px 10px -15px #111; 
+`
+export const ShadowLeft = styled.div`
+    box-shadow: -15px 0px 10px -15px #111;
+`
+export const FullShadow = styled.div`
+    box-shadow: 0px 0px 40px 0px #00000040;
+`
+export const WarningShadow = styled.div`
+    box-shadow: 4px 4px 4px 4px #00000040;
+`
