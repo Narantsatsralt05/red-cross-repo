aa6233062399f6045675f0c3579741e710d3ea0a
Bilguun2004 <bilguun8689@gmail.com>
2022-04-18 18:26:10 +0800

    user info 80%

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,26 +1,18 @@
 import React, { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { useAuthContext } from '../providers/authContext';
-import UserInfo from './UserInfo';
-import { Stepper } from '../components';
 
 const Home = () => {
-  const { user } = useAuthContext();
+    const { user } = useAuthContext();
 
-  const router = useRouter();
+    const router = useRouter();
 
-  useEffect(() => {
-    if (user === null) {
-      router.push('/login');
-    }
-  }, [user]);
+    useEffect(() => {
+        if (user === null) {
+            router.push('/login');
+        }
+    }, [user]);
 
-  const data = {
-    header: 'ХЭРЭГЛЭГЧИЙН ДЭЛГЭРЭНГҮЙ МЭДЭЭЛЭЛ БҮРТГЭХ',
-    headers: ['ХУВЬ ХҮНИЙ ТАЛААРХ МЭДЭЭЛЭЛ', 'САЙН ДУРЫН АЖЛЫН ТУРШЛАГА', 'ХАМРАГДСАН СУРГАЛТ', 'УР ЧАДВАР'],
-    body: [<UserInfo />, <>2</>, <>3</>, <>4</>],
-  };
-
-  return <Stepper data={data} />;
+    return <>Thats home</>;
 };
 export default Home;
