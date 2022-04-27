f3109e1161f922a34d0a0e215af60c039db9f54e
Mirgalim <mirgalim@iMac-20s-iMac.local>
2022-04-27 17:21:43 +0800

    fix dropdown home

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,34 +2,18 @@
 import React, { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { useAuthContext } from '../providers/authContext';
-import Dropdown from '../components/common/select';
+
 const Home = () => {
     const { user } = useAuthContext();
 
     const router = useRouter();
-    const option = [
-        { 'text' : '1'},
-        { 'text' : '2'},
-        { 'text' : '3'},
-        { 'text' : '4'}
-    ]
+
     useEffect(() => {
         if (user === null) {
             router.push('/login');
         }
     }, [user]);
 
-    return <>
-    <div style={{display:'flex', justifyContent:'space-between', width:'2000px'}}>
-    <Dropdown arr={option}></Dropdown>
-    <Dropdown arr={option}></Dropdown>
-    <Dropdown arr={option}></Dropdown>
-    <Dropdown arr={option}></Dropdown>
-    </div>
-    <div>
-    <Dropdown arr={option}></Dropdown>
-    <Dropdown arr={option}></Dropdown>
-    </div>
-    </>;
+    return <>Thats home</>;
 };
 export default Home;
