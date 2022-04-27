277e98a00b26fbc8a376f133b3c5ce5a6b2b6a81
Orgil <F4stella@CC0096.local>
2022-04-27 18:34:01 +0800

    117-admin-home-page

---
+++ src/components/core/index.js
@@ -0,0 +1,12 @@
+
+export * from "./button";
+export * from "./input";
+export * from "./position";
+export * from "./border";
+export * from "./center";
+export * from "./input";
+export * from "./margin";
+export * from "./stack";
+export * from "./text";
+export * from "./padding";
+export * from "./box";
