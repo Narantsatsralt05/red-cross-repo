c48b4732279e4425f487f28c4cbbabf4900feebe
Tuugii <87753063+Tuugii0507@users.noreply.github.com>
2022-05-02 16:51:36 +0800

    themFontSize (#146)
    Co-authored-by: Tuugii <tuugii@iMac2021-08s-iMac.local>

--- src/components/core/text.jsx
+++ src/components/core/text.jsx
@@ -1,14 +1,16 @@
-import style from 'styled-components';
+import styled from 'styled-components';
+import { FontSizes } from './fontSizes';
 
-export const Text = style.div`
- color:${({ color }) => color};
- font-size:${({ fontSize }) => fontSize};
- font-family:${({ fontFamily }) => fontFamily};
- font-style: ${({ fontStyle }) => fontStyle};
- font-weight: ${({ fontWeight }) => fontWeight};
- line-height:${({ lineHeight }) => lineHeight};
- cursor: ${({ cursor }) => cursor};
- text-transform: ${({textTransform}) => textTransform};
- flex-direction: row;
- display: flex;
+export const Text = styled.text`
+  @media (min-width: 541px) {
+    font-size: ${({ type }) => FontSizes.onDesktop[type]};
+  }
+  @media (max-width: 540px) {
+    font-size: ${({ type }) => FontSizes.onMobile[type]};
+  }
+  color: ${({ color }) => color};
+  font-family: ${({ fontFamily }) => (fontFamily ? fontFamily : 'Roboto')};
+  font-style: ${({ fontStyle }) => fontStyle};
+  cursor: ${({ cursor }) => cursor};
+  text-transform: ${({ textTransform }) => textTransform};
 `;
