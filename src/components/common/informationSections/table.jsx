24b28cfea3328748bfb3782ffd01ae70c48e045e
Tuugii <tuugii@iMac2021-08s-iMac.local>
2022-05-06 18:42:14 +0800

    updateSections

--- src/components/common/informationSections/table.jsx
+++ src/components/common/informationSections/table.jsx
@@ -6,6 +6,7 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import { useTheme } from '../../../theme/theme';
 import { Margin, Text, Stack } from '../..';
+import TableCell from '@mui/material/TableCell';
 import styled from 'styled-components';
 
 export const InformationTable = ({ admin, title, row, body }) => {
@@ -84,7 +85,35 @@ export const InformationTable = ({ admin, title, row, body }) => {
       display: none;
     }
   `;
+  const tableBodyCell = {
+    border: 'none',
+    fontStyle: 'normal',
+    padding: '10px',
+    width: '20vw',
+    align: 'left',
+    first: {
+      border: 'none',
+      fontStyle: 'normal',
+      padding: '10px',
+      width: '20vw',
+      align: 'left',
+      color: color.primary.blue,
+    },
+  };
 
+  const Hover = styled.text`
+    &:hover {
+      color: ${color.secondary.blue};
+    }
+    &:active {
+      color: ${color.disabled.blue};
+    }
+  `;
+  const haha = () => {
+    console.log('nasaa teneg');
+    //nasaa onClick bichih tul tur turshiltaar yvuulav
+    //nasaa function ner edree uurchluurei
+  };
   return (
     <Stack>
       <Margin size={[35, 30, 10, 10]}>
@@ -102,7 +131,25 @@ export const InformationTable = ({ admin, title, row, body }) => {
               </TableHead>
             </Margin>
             <Stack width="100%" height="1px" bg={color.secondary.white} />
-            <TableBody>{body}</TableBody>
+            <TableBody>
+              {body.map((el) => {
+                return (
+                  <Margin size={[0, 0, 5, 10]}>
+                    <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                      {el.map((element, index) => {
+                        return (
+                          <TableCell style={index === 0 ? tableBodyCell.first : tableBodyCell}>
+                            <Text type={index === 0 ? 'T2Caps' : 'T2'} onClick={index === 0 ? haha : null}>
+                              {index === 0 ? <Hover>{element}</Hover> : element}
+                            </Text>
+                          </TableCell>
+                        );
+                      })}
+                    </TableRow>
+                  </Margin>
+                );
+              })}
+            </TableBody>
           </Table>
         </TableContainer>
       </Margin>
@@ -111,4 +158,6 @@ export const InformationTable = ({ admin, title, row, body }) => {
 };
 
 //<SkillTable admin={true} />
-{/* <SkillTable admin={false} /> */}
+{
+  /* <SkillTable admin={false} /> */
+}
