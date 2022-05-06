6222a543517d3b47b6c2d60aef8c1c2d2be1b4e9
Tuugii <87753063+Tuugii0507@users.noreply.github.com>
2022-05-06 16:03:44 +0800

    installRobotoStyle (#161)
    Co-authored-by: Tuugii <tuugii@iMac2021-08s-iMac.local>

--- src/components/core/text.jsx
+++ src/components/core/text.jsx
@@ -1,16 +1,29 @@
 import styled from 'styled-components';
-import { FontSizes } from './fontSizes';
+import { FontSizes, fontWeight, lineHeight } from './fontSizes';
 
 export const Text = styled.text`
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap');
   @media (min-width: 541px) {
     font-size: ${({ type }) => FontSizes.onDesktop[type]};
   }
   @media (max-width: 540px) {
     font-size: ${({ type }) => FontSizes.onMobile[type]};
   }
+  @media (min-width: 541px) {
+    font-weight: ${({ type }) => fontWeight.onDesktop[type]};
+  }
+  @media (max-width: 540px) {
+    font-weight: ${({ type }) => fontWeight.onMobile[type]};
+  }
+  @media (min-width: 541px) {
+    line-height: ${({ type }) => lineHeight.onDesktop[type]};
+  }
+  @media (max-width: 540px) {
+    line-height: ${({ type }) => lineHeight.onMobile[type]};
+  }
   color: ${({ color }) => color};
   font-family: ${({ fontFamily }) => (fontFamily ? fontFamily : 'Roboto')};
-  font-style: ${({ fontStyle }) => fontStyle};
+  font-style: ${({ fontStyle }) => fontStyle ? fontStyle : "normal"};
   cursor: ${({ cursor }) => cursor};
   text-transform: ${({ textTransform }) => textTransform};
 `;
