c0d7561d9257344903425b1c214f4a452e44cc4e
Mirgalim <97152700+Mirgalim@users.noreply.github.com>
2022-05-06 18:05:06 +0800

    Dropdown bug fix (#163)
    * dropdown bug fix
    
    * dropdown bug fix
    
    * dropdown bug fix
    
    Co-authored-by: Mirgalim <mirgalim@iMac-20s-iMac.local>

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -10,7 +10,7 @@ export * from "./leftbar";
 export * from "./loading";
 export * from "./page-input-task";
 export * from "./paginationtext";
-export * from "./select";
+export * from "./dropdown";
 export * from "./status";
 export * from "./informationSections/sections";
 
