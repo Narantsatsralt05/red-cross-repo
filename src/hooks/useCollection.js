a374bf7afcbacfa6036eaf55b9b73c5f730ec9e2
Tuugii <tuugii@iMac2021-08s-iMac.local>
2022-05-11 18:08:21 +0800

    dashboard last added person section and fix data-table.jsx

--- src/hooks/useCollection.js
+++ src/hooks/useCollection.js
@@ -1,21 +1,28 @@
 import { useState, useEffect } from 'react';
 import { db } from '.';
 
-export const useCollection = (path) => {
-  const [dataId, setDataId] = useState([]);
+export const useCollection = (path, sorted = false) => {
   const [data, setData] = useState([]);
+  const [dataId, setDataId] = useState([]);
   useEffect(() => {
     if (db) {
       const arrData = [];
-      const ID = []
-      db.collection(`${path}`).onSnapshot((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-          arrData.push(doc.data());
-          ID.push(doc.id)
+      const ID = [];
+
+      if (db) {
+        let col = db.collection(`${path}`);
+        if (sorted) {
+          col = col.orderBy('timestamp', 'desc').limit(30);
+        }
+        col.onSnapshot((querySnapshot) => {
+          querySnapshot.forEach((doc) => {
+            arrData.push(doc.data());
+            ID.push(doc.id);
+          });
+          setData(arrData);
+          setDataId(ID);
         });
-        setData(arrData);
-        setDataId(ID)
-      });
+      }
     }
   }, [path, db]);
 
