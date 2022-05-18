0cf352d18fc8304e339ebcd23be802227f335df3
F4stella <f4stella@iMac-17s-iMac.local>
2022-05-18 18:08:06 +0800

    189 registered page update

--- src/components/common/registered.jsx
+++ src/components/common/registered.jsx
@@ -14,10 +14,10 @@ export const Registered = ({ e }) => {
     0,
   ];
   return (
-    <Stack width="100%" height="100%" bg="#FAFAFA" justifyContent="space-between" direction="column">
+    <Stack width="94%" height="100%" justifyContent="space-between" direction="column">
       <Stack width="100%" height="7%" justifyContent="flex-end" alignItems="center">
         <Margin size={[0, 80, 0, 0]}>
-          <Stack justifyContent="space-evenly" gap="25">
+          <Stack justifyContent="space-evenly" gap="35">
             <Image src={shape}></Image>
             <Styledinput width="350px" placeholder="хайх"></Styledinput>
             <Image src={excel}></Image>
@@ -27,40 +27,40 @@ export const Registered = ({ e }) => {
       <Image src={upline}></Image>
       <Stack
         width="100%"
-        height="6%"
-        bg="#D9D9D9"
+        height="7%"
+        bg={color.primary.grey}
         justifyContent="space-evenly"
         alignItems="center"
         justifyContent="center"
-      >
+         >
         <Stack width="14%">
-          <Text type="T1">Регистрийн дугаар</Text>
+          <Text type="T4Spaced">Регистрийн дугаар</Text>
         </Stack>
         <Stack width="12%">
           <Margin size={[0, 0, 0, 23]}>
-            <Text type="T1">Овог</Text>
+            <Text type="T4Spaced">Овог</Text>
           </Margin>
         </Stack>
         <Stack width="11%">
-          <Text type="T1">Нэр</Text>
+          <Text type="T4Spaced">Нэр</Text>
         </Stack>
         <Stack width="5%">
-          <Text type="T1">Нас</Text>
+          <Text type="T4Spaced">Нас</Text>
         </Stack>
         <Stack width="7%">
-          <Text type="T1">Хүйс</Text>
+          <Text type="T4Spaced">Хүйс</Text>
         </Stack>
         <Stack width="18%">
-          <Text type="T1">Төрөл</Text>
+          <Text type="T4Spaced">Төрөл</Text>
         </Stack>
         <Stack width="11%">
-          <Text type="T1">Утас</Text>
+          <Text type="T4Spaced">Утас</Text>
         </Stack>
         <Stack width="11%">
-          <Text type="T1">Элссэн огноо</Text>
+          <Text type="T4Spaced">Элссэн огноо</Text>
         </Stack>
         <Stack width="10%">
-          <Text type="T1" style={{ textAlign: 'center' }}>
+          <Text type="T4Spaced" style={{ textAlign: 'center' }}>
             Гишүүнчлэл огноо
           </Text>
         </Stack>
@@ -68,34 +68,34 @@ export const Registered = ({ e }) => {
       <Stack width="100%" height="85%" direction="column" justifyContent="space-between" style={{ overflow: 'auto' }}>
         {a.map((el) => {
           return (
-            <Margin size={[0, 0, 25, 0]}>
+            <Margin size={[0, 0, 28, 0]}>
               <Stack width="100%" height="190%" bg="white" justifyContent="space-between">
                 <Stack width="14%" alignItems="center">
                   <Margin size={[0, 0, 0, 10]}>
-                    <Text type="T2" color={color.primary.blue}>
+                    <Text type="ThinH2" color={color.primary.blue}>
                       УК94092597
                     </Text>
                   </Margin>
                 </Stack>
                 <Stack width="12%" alignItems="center">
                   <Margin size={[0, 0, 0, 30]}>
-                    <Text type="T2">Нямсамбуу-Cайхан</Text>
+                    <Text type="ThinH2">Нямсамбуу-Cайхан</Text>
                   </Margin>
                 </Stack>
                 <Stack width="11%" alignItems="center">
-                  <Text type="T2">Даваа-Самбуу</Text>
+                  <Text type="ThinH2">Даваа-Самбуу</Text>
                 </Stack>
                 <Stack width="5%" alignItems="center">
-                  <Text type="T2">28</Text>
+                  <Text type="ThinH2">28</Text>
                 </Stack>
                 <Stack width="7%" alignItems="center">
-                  <Text type="T2">эрэгтэй</Text>
+                  <Text type="ThinH2">эрэгтэй</Text>
                 </Stack>
                 <Stack width="18%" alignItems="center">
                   <Border borderRadius="5px" borderColor={color.secondary.lightblue}>
                     <Stack bg={color.secondary.lightblue}>
                       <Padding size={[1, 10, 1, 10]}>
-                        <Text type="T2">Алтан гишүүн</Text>
+                        <Text type="ThinH2">Алтан гишүүн</Text>
                       </Padding>
                     </Stack>
                   </Border>
@@ -103,23 +103,23 @@ export const Registered = ({ e }) => {
                     <Border borderRadius="5px" borderColor={color.secondary.lightgreen}>
                       <Stack bg={color.secondary.lightgreen}>
                         <Padding size={[1, 10, 1, 10]}>
-                          <Text type="T2">СДИ</Text>
+                          <Text type="ThinH2">СДИ</Text>
                         </Padding>
                       </Stack>
                     </Border>
                   </Margin>
                 </Stack>
                 <Stack width="11%" alignItems="center">
-                  <Text type="T2">88109325</Text>
+                  <Text type="ThinH2">88109325</Text>
                 </Stack>
                 <Stack width="11%" alignItems="center">
-                  <Text type="T2">2018.05.01</Text>
+                  <Text type="ThinH2">2018.05.01</Text>
                 </Stack>
                 <Stack width="10%" alignItems="center">
                   <Margin size={[0, 10, 0, 0]}>
                     <Image src={warning}></Image>
                   </Margin>
-                  <Text type="T2" color={color.secondary.orange}>
+                  <Text type="ThinH2" color={color.secondary.orange}>
                     2019.05.01
                   </Text>
                 </Stack>
--- src/pages/admin/registeredUsers.jsx
+++ src/pages/admin/registeredUsers.jsx
@@ -6,26 +6,23 @@ import green from "../../assets/icon/greenhand.svg"
 import hamarrow from "../../assets/icon/hamarrow.svg"
 import Image from "next/image";
 export const Registeredusers = () => {
-    return <Stack bg={color.primary.grey} width="100vw" height="100vh" direction="column" justifyContent="space-between">
+    return <Stack  bg={color.primary.background} width="100vw" height="100vh" direction="column" justifyContent="space-between">
         <Stack width="100%" height="13%" direction="column">
         <Header></Header>
-        <Stack bg={color.secondary.grey} width="100vw" height="6vh" style={{boxShadow:"0px  13px  10px  -15px  #111"}}>
+        <Stack bg={color.secondary.lightgrey} width="100vw" height="6vh" style={{boxShadow:"0px  13px  10px  -15px  #111"}}>
             <Margin size={[10,0,0,30]}>
             <Stack  alignItems="center" width="20vw"height="80%" direction="column" justifyContent="space-evenly">
                 <Text type="T1">
                 БҮРТГЭЛТЭЙ ХЭРЭГЛЭГЧИЙН МЭДЭЭЛЭЛ
                 </Text>
-                <Margin size={[0,30,0,0]}>
-                    <Stack width="17vw" style={{borderTop:"1px blue solid"}}>
-           
-                    </Stack>
-                </Margin>
+                <Stack width="17vw" style={{borderTop:"1px blue solid"}}></Stack>
+                
             </Stack>
             </Margin>
         </Stack>
         </Stack>
-        <Stack bg={color.primary.grey} width="100vw" height="86vh" justifyContent="space-between" >
-            <Stack  bg={color.secondary.lightgrey} width="5vw" height="100%" style={{boxShadow:"13px  0px  10px  -15px  #111"}} direction="column"justifyContent="space-between">
+        <Stack bg={color.primary.background} width="100vw" height="86vh" justifyContent="space-evenly" >
+            <Stack  bg={color.secondary.lightgrey} width="4vw" height="100%" style={{boxShadow:"13px  0px  10px  -15px  #111"}} direction="column"justifyContent="space-between">
                 <Stack  width="100%" height="30vh" justifyContent="space-evenly" alignItems="center" direction="column">
                     <Image src={hands}></Image>
                     <Image src={group}></Image>
