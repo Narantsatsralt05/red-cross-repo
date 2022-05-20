92a2456f344d7a104ae79deeabded395fe88a4cd
nasanbat II <nasaa@iMac2021-07s-iMac.local>
2022-05-20 18:48:10 +0800

    userIOnfo

--- src/pages/user/home.jsx
+++ src/pages/user/home.jsx
@@ -1,27 +1,6 @@
-// import React, { useEffect } from 'react';
-// import { useRouter } from 'next/router';
-// import { useAuthContext } from '../../common/context/AuthContext';
-
-// const UserHome = () => {
-//     const { user } = useAuthContext();
-//     const router = useRouter();
-
-//     useEffect(() => {
-//         if (user === null) {
-//             router.push('/login');
-//         }
-//     }, [user]);
-//     return <>thats user home</>;
-// };
-// export default UserHome;
-
-
-
-
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { useAuthContext } from '../../common/context/AuthContext';
-import ProfileHome from './ProfileHome'
 
 const UserHome = () => {
     const { user } = useAuthContext();
@@ -32,7 +11,6 @@ const UserHome = () => {
             router.push('/login');
         }
     }, [user]);
-    return <ProfileHome/>;
+    return <>thats user home</>;
 };
 export default UserHome;
-
